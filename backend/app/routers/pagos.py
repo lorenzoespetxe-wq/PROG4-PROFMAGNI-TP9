9e8f829470f1fcd2e49f4d58f9c910581e77c0ca
Lorenzo <lorenzoespetxe@gmail.com>
2026-06-08 15:38:29 -0300

    Llegamos a la pantalla de MP. No aprueba los pagos aún.

--- backend/app/routers/pagos.py
+++ backend/app/routers/pagos.py
@@ -1,17 +1,21 @@
 import os
+from pathlib import Path
 
 import mercadopago
 from dotenv import load_dotenv
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
-load_dotenv(override=True)
+BASE_DIR = Path(__file__).resolve().parent.parent.parent  # llega a /backend
+load_dotenv(BASE_DIR / ".env", override=True)
+
 
 router = APIRouter(prefix="/pagos", tags=["pagos"])
 
 # IMPORTANTE: Reemplazar con el Access Token de prueba de Mercado Pago
 # Lo obtienes en: https://www.mercadopago.com.ar/developers/panel/credentials
 ACCESS_TOKEN = os.getenv("MP_ACCESS_TOKEN")
+print("TOKEN LEIDO:", ACCESS_TOKEN)  # verficamos que el token se lea bien
 sdk = mercadopago.SDK(ACCESS_TOKEN)
 
 
@@ -35,22 +39,24 @@ def crear_preferencia(solicitud: SolicitudPago):
             ],
             # Aquí configuras las URLs de retorno. Si utilizas ngrok,
             # reemplaza "http://localhost:5173" por tu URL pública de ngrok.
-            "back_urls": {
+            "back_url": {
                 "success": "http://localhost:5173/pago-exitoso",
                 "failure": "http://localhost:5173/pago-fallido",
                 "pending": "http://localhost:5173/pago-pendiente",
             },
-            "auto_return": "approved",
+            # "auto_return": "approved", esto lo rompe en pruebas con locahost parece
         }
 
         # Envía la solicitud a la API de Mercado Pago
         preference_response = sdk.preference().create(preference_data)
         preference = preference_response["response"]
+        print("RESPUESTA MP:", preference_response)  # ← agregá esto
 
         # Retorna el ID de la preferencia y el enlace de pago (init_point)
         return {"id": preference["id"], "init_point": preference["init_point"]}
 
     except Exception as e:
+        print("ERROR DETALLADO:", str(e))  # ← agregá esto
         raise HTTPException(
             status_code=500, detail=f"Error al conectar con Mercado Pago: {str(e)}"
         )
